2fe5a9ca5db2c74f327229bfdeba56910cafc507
Jimbo9526 <giuseppegabrieledileo95@gmail.com>
2022-04-06 15:38:54 +0200

    token nuovo

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -26,7 +26,7 @@ export class TokenInterceptor implements HttpInterceptor {
             .set(
               'Authorization',
               'Bearer ' +
-                'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0NzUwNzE5OCwiZXhwIjoxNjQ4MzcxMTk4fQ.hDmA5CnOrvWrGOq_cOiMaptYKXUvU186b5BoxNRqjLrDfpByPrdIacV0GFkBA_U7lWVqK4LekBVXXAskE_ft6Q'
+                'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0OTI1MjIzMSwiZXhwIjoxNjUwMTE2MjMxfQ.cvvJqf-xOZYpXstbmIzg-nwkZD_WLgfq8ahLOnL2YK6L5NC3O_iFr0ZMfZeB4LZ9WGictdRqWco9h5iPDGC-3A'
             )
             .set('X-TENANT-ID', 'fe_0721a'),
         });
